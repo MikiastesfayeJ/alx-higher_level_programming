85d01d532058645db3ad6bfc751b5854e91cd68c
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-06-05 00:48:41 +0300

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,10 +10,12 @@ int is_palindrome(listint_t **head)
     const listint_t *current;
     const listint_t *tail;
     unsigned int n = 0;
+
     //Get length
     //Use a function to get a node on a specific index
     //check the first to the last and, second to second last ....
     //return false if one check was False
     //return true at the end (if all passed the checks)
+
     return (n);
 }
