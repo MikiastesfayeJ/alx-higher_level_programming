8800203b003284b4815ed0ca807846f9497486b5
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-07-04 23:29:49 +0300

    task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""MyInt"""
+"""Module 100-my_int.
+Creates a class that inherits from int.
+"""
 
 
 class MyInt(int):
-    """inherits from int, has == and != inverted"""
+    """Class inheriting from int,
+    But reverses the behavior of != and ==.
+    """
 
-    def __eq__(self, value):
-        """overiding == operator"""
-        return self.real != value
+    def __eq__(self, other):
+        """Equality becomes inequality."""
 
-    def __ne__(self, value):
-        """overriding != operator"""
-        return self.real == 
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Inequality becomes equality."""
+
+        return super().__eq__(other)
