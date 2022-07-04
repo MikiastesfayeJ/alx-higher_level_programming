7851401e86e10e6e50de19e6dec3836d71ffea26
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-07-04 23:28:46 +0300

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-"""
-This module implements a Square object
+"""Module: 11-square
+This is a class Square that inherits from Rectangle class as the base class
 """
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """This is a rectangle class
+    It has the following private attribute:
+        size = size
+    It inherits the integer validator from the Rectangle class
+    """
+
     def __init__(self, size):
-        """initialization
-        Args:
-            size (int): size
+        """Validates the following attributes:
+            - size
         """
+
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
+    
     def __str__(self):
-        """string representation
-        Returns:
-            str: string
-        """
-        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
+        """Returns a format string of the attribute size"""
+
+        return("[Square] {}/{}".format(self.__size, self.__size))
+    
+    def area(self):
+        """Returns the area of the Square instance"""
+
+        return self.__size **2
