6e179ce9d896d728113d0a88ba94d2446eff3e4f
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-07-04 23:20:47 +0300

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
-This module implements  a function that returns True if the object
-is an instance of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.
+4-inherits_from.py
 """
 
 
-def inherits_from(obj, a_class) -> bool:
-    """implementation
+def inherits_from(obj, a_class):
     """
-    return type(obj) != a_class and issubclass(type(obj), a_class)
+    returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified
+    class ; otherwise False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
