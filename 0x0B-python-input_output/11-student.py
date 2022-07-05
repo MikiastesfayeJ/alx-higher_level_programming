6800d1ef14bb1e6eef820d7f0d7250db4606ac97
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-07-05 23:52:00 +0300

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
-"""
-Defines Student class
-"""
+import sys
 
 
-class Student():
-    """
-        A Student class
-    """
-    def __init__(self, first_name, last_name, age):
-        """
-            defines first_name, last_name, age
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+def print_status():
+    '''
+        Printing the status of the request
+    '''
+    counter = 0
+    size = 0
+    file_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
 
-    def to_json(self, attrs=None):
-        """
-            retrieves a dictionary representation
-            of a Student instance
-        """
-        if type(attrs) is list and all([type(s) == str for s in attrs]):
-            return {i: j for i, j in self.__dict__.items() if i in attrs}
-        return(self.__dict__)
+    for l in sys.stdin:
+        line = l.split()
+        try:
+            size += int(line[-1])
+            code = line[-2]
+            status_codes[code] += 1
+        except:
+            continue
+        if counter == 9:
+            print("File size: {}".format(size))
+            for key, val in sorted(status_codes.items()):
+                if (val != 0):
+                    print("{}: {}".format(key, val))
+            counter = 0
+        counter += 1
+    if counter < 9:
+        print("File size: {}".format(size))
+        for key, val in sorted(status_codes.items()):
+            if (val != 0):
+                print("{}: {}".format(key, val))
 
-    def reload_from_json(self, json):
-        """
-            reload json object
-        """
-        for i, j in json.items():
-            self.__dict__[i] = j
+
+if __name__ == "__main__":
+    print_status()
