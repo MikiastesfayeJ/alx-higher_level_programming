fae47c1a0608b3516c4c6966c9d78d7853edeeed
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-07-06 02:19:46 +0300

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""
-    5-save_to_json_file: save_to_json_file()
-"""
-
-
+"""A function that writes an Object to
+a text file, using a JSON representation"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """
-        writes an object to a text file using JSON rep.
-        Args:
-            my_obj (object): object to be serialized.
-            filename (str): name of file where string is stored.
-    """
-    with open(filename, "w") as j_file:
-        json.dump(my_obj, j_file)
+    """Script to write an object to text using
+json representation"""
+
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
