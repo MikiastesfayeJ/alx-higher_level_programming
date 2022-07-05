37e909c79d70d44d7627ab0da59acd1435e98b6b
MikiastesfayeJ <57168613+MikiastesfayeJ@users.noreply.github.com>
2022-07-06 02:20:25 +0300

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""
-    6-load_from_json_file: load_from_json_file()
-"""
-
-
+"""import json modules"""
 import json
 
 
 def load_from_json_file(filename):
-    """
-        loads an object to from JSON file.
-        Args:
-            filename (str): name of file.
-    """
-    with open(filename, "r") as j_file:
-        my_obj = json.load(j_file)
-        return my_obj
+    """Script to create an object from json file"""
+
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
